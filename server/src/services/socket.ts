acc9208e5d18316068fbf3796b08838741bf35d7
lee pham <thehienpv@gmail.com>
2021-08-07 15:49:03 +0700

    update origin

--- server/src/services/socket.ts
+++ server/src/services/socket.ts
@@ -15,7 +15,7 @@ const app = require('express')()
 const server = createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: process.env.SOCKETIO_ALLOW_ORIGIN,
     methods: ['GET', 'POST'],
     credentials: true,
   },
